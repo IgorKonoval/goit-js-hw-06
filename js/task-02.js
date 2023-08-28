7bdd1c74abe0546d1d4539c1c11197c63d750ddb
Igor Konoval <kip_y@sfa.org.ua>
2023-08-29 00:56:02 +0300

    Home work vol.9

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const createLi = document.querySelector("#ingredients");
+
+ingredients.forEach((element) => {
+  const liItem = document.createElement("li");
+
+  liItem.textContent = element;
+  liItem.classList.add("item");
+  createLi.append(liItem);
+});
+console.log(createLi);
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const changeColor = document.querySelector(".change-color");
+const bodyColor = document.querySelector("body");
+const nameColor = document.querySelector(".color");
+
+changeColor.addEventListener("click", onClick);
+
+function onClick() {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  nameColor.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
